acb52c2ea4014884b27dbff8a55af2707c533b14
jonah simpson <jonahs99@gmail.com>
2026-07-19 17:16:43 -0400

    add a service file (untested)

--- logserver/src/main.rs
+++ logserver/src/main.rs
@@ -1,6 +1,6 @@
 use axum::{Router, routing::get_service};
 use mime::TEXT_PLAIN;
-use std::{fs::OpenOptions, io::Write};
+use std::{env::var, fs::OpenOptions, io::Write};
 use time_format::format_iso8601_utc;
 use tower_http::services::ServeFile;
 
@@ -8,18 +8,22 @@ use tower_http::services::ServeFile;
 async fn main() {
     let router = Router::new().route(
         "/log",
-        get_service(ServeFile::new_with_mime("./log", &TEXT_PLAIN)).post(log),
+        get_service(ServeFile::new_with_mime(log_path(), &TEXT_PLAIN)).post(log),
     );
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
+}
+
+fn log_path() -> String {
+    var("MUD_LOG_PATH").unwrap_or_else(|_| "./log".to_string())
 }
 
 async fn log(data: String) {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open("./log")
+        .open(log_path())
         .expect("could not open log file");
 
     let time = format_iso8601_utc(time_format::now().unwrap()).unwrap();
--- mudthings/mudlogger/mudlogger.ino
+++ mudthings/mudlogger/mudlogger.ino
@@ -1,31 +1,95 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 
 // Defines SSID, PWD, LOG_URL
 #include <secrets.h>
 
+#define SDA_PIN 8
+#define SCL_PIN 9 
+
+#define PIN_MUX_S1 13 
+#define PIN_MUX_S2 12 
+#define PIN_MUX_S3 11 
+
+#define K_SAMPLE_COUNT 10
+
+Adafruit_INA219 ina219;
+
+float busvoltage = 0;
+float current_mA = 0;
+float power_mW = 0;
+
+namespace mux {
+  void setup() {
+    pinMode(PIN_MUX_S1, OUTPUT);
+    pinMode(PIN_MUX_S2, OUTPUT);
+    pinMode(PIN_MUX_S3, OUTPUT);
+  }
+  void selectInput(uint8_t val) {
+    digitalWrite(PIN_MUX_S1, val & 0b001);
+    digitalWrite(PIN_MUX_S2, val & 0b010);
+    digitalWrite(PIN_MUX_S3, val & 0b100);
+  }
+}
+
 void setup() {
-    Serial.begin(9600);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Serial.begin(115200);
 
-    WiFi.begin(SSID, PWD);
+  mux::setup();
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+  // initialize ina219 with default measurement range of 32V, 2A
+  if (!ina219.begin()) {
+    Serial.println("INA219 not found");
+    while (1);
+  }
+
+  // ina219.setCalibration_32V_2A();    // set measurement range to 32V, 2A  (do not exceed 26V!)
+  // ina219.setCalibration_32V_1A();    // set measurement range to 32V, 1A  (do not exceed 26V!)
+  // ina219.setCalibration_16V_400mA(); // set measurement range to 16V, 400mA
+
+  WiFi.begin(SSID, PWD);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.print("connected!");
+
+  String line = "";
+  for (uint8_t input=0; input<8; input++) {
+    mux::selectInput(input);
+    delay(2); // let settle
+
+    // read data from ina219
+    busvoltage = 0;
+    current_mA = 0;
+    power_mW = 0;
+    for (uint8_t i=0; i < K_SAMPLE_COUNT; i++) {
+      busvoltage += ina219.getBusVoltage_V();
+      current_mA += ina219.getCurrent_mA();
+      power_mW += ina219.getPower_mW();
+      delay(2);
     }
+    if (busvoltage!=0) busvoltage /= K_SAMPLE_COUNT;
+    if (current_mA!=0) current_mA /= K_SAMPLE_COUNT;
+    if (power_mW!=0) power_mW /= K_SAMPLE_COUNT;
 
-    Serial.print("connected!");
+    line += busvoltage;
+    line += "\t";
+  }
+
+  HTTPClient http;
+  String url = LOG_URL;
+  http.begin(url.c_str());
+  http.POST(line);
+  http.end();
+
+  esp_sleep_enable_timer_wakeup(5000000);
+  esp_deep_sleep_start();
 }
 
-void loop() {
-    delay(1000);
-
-    String url = LOG_URL;
-    String data = "hello from mudthing :)";
-
-    HTTPClient http;
-    http.begin(url.c_str());
-    http.POST(data);
-    http.end();
-}
+void loop() { }
 
