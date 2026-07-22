f6224cc285ec3e13d43f4e02209bc434215f11cf
jonah simpson <jonahs99@gmail.com>
2026-07-22 16:51:07 -0400

    start on chart

--- logserver/src/main.rs
+++ logserver/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{Router, routing::get_service};
+use axum::{
+    Router,
+    http::{HeaderMap, HeaderValue, StatusCode, header::CONTENT_TYPE},
+    response::IntoResponse,
+    routing::{get, get_service},
+};
 use mime::TEXT_PLAIN;
 use std::{env::var, fs::OpenOptions, io::Write};
 use time_format::format_iso8601_utc;
@@ -6,10 +11,13 @@ use tower_http::services::ServeFile;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route(
-        "/log",
-        get_service(ServeFile::new_with_mime(log_path(), &TEXT_PLAIN)).post(log),
-    );
+    let router = Router::new()
+        .route(
+            "/log",
+            get_service(ServeFile::new_with_mime(log_path(), &TEXT_PLAIN)).post(log),
+        )
+        .route("/chart", get(get_chart))
+        .route("/chart/plotly-3.6.0.min.js", get(get_plotly));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
@@ -31,4 +39,18 @@ async fn log(data: String) {
 
     file.write_all(line.as_bytes())
         .expect("could not append to log file");
+}
+
+async fn get_chart() -> impl IntoResponse {
+    let headers = HeaderMap::from_iter([(CONTENT_TYPE, HeaderValue::from_static("text/html"))]);
+    (StatusCode::OK, headers, include_str!("../chart/chart.html"))
+}
+
+async fn get_plotly() -> impl IntoResponse {
+    let headers = HeaderMap::from_iter([(CONTENT_TYPE, HeaderValue::from_static("text/html"))]);
+    (
+        StatusCode::OK,
+        headers,
+        include_str!("../chart/plotly-3.6.0.min.js"),
+    )
 }
